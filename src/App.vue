efb90eb8fe2b9d57aca2fd8a4b87a6334c0271ec
Efe Aydemir <delidoluefe32@gmail.com>
2023-01-06 00:38:55 +0300

    Created

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="root" class="text-white text-center">
+    <div id="projects" class="flex py-20 flex-col items-center justify-center">
+      <h1 class="text-3xl">Projects</h1>
+      <div class="flex flex-col items-center justify-center gap-10 pt-10">
+        <WcButton btitle="Number Game" @click="golink('https://vue-random-number.vercel.app/')">
+          Number Game
+        </WcButton>
+
+        <WcButton btitle="Pasword Generator" @click="golink('https://vue-password-creater.vercel.app/')">
+          Pasword Generator
+        </WcButton>
+
+        <WcButton btitle="Weather App" @click="golink('https://vue-weather-app-omega.vercel.app/')">
+          Weather App
+        </WcButton>
+
+        <WcButton btitle="Quiz App" @click="golink('https://mon-quiz-app.vercel.app/')">
+          Quiz App
+        </WcButton>
+
+        <WcButton btitle="Admin Panel" @click="golink('https://admin-panel-two-zeta.vercel.app/#/')">
+          Basic Admin Panel
+        </WcButton>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+<script setup>
+import WcButton from '@/comp/WcB.vue'
+function golink(link) {
+  window.open(link)
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: rgba(0, 0, 0, 0.795);
 }
 </style>
---
+++ src/comp/WcB.vue
@@ -0,0 +1,26 @@
+<template>
+    <button :title="props.btitle"
+    class="
+    text-lg
+    py-1
+    px-10
+    rounded-md
+    bg-white
+    text-black
+    transition-all
+    duration-300
+    hover:shadow-xl
+    hover:shadow-black
+    hover:px-12
+    hover:rounded-lg
+    hover:font-bold
+    "
+    ><slot></slot></button>
+</template>
+
+
+
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps(['btitle'])
+</script>
